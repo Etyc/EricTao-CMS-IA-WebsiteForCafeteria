9a874ac6e9022d90d9a72c65ff2eb8c37a60df91
Etyc <yctao_eric@qibaodwight.org>
2018-09-29 14:19:44 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
  
-$link = mysqli_connect("localhost","qdhseri1","Yie26e0aV3","qdhseri1_mydatabase");
+$link = mysqli_connect("localhost","user","","test");
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -12,7 +12,7 @@ else{
     echo " connected to MySQL." ;
     
 }
-$sql = "INSERT INTO account (Email, AccountName, Password) VALUES ('$_POST[Email]', '$_POST[AccountName]', '$_POST[Password]')" ;
+$sql = "INSERT INTO account (email, password) VALUES ('$_POST[Email]', '$_POST[Password]')" ;
 echo $sql;
 if (!mysqli_query($link, $sql)){
     echo "inserting into database failed";/// add something to show that there was a problem
